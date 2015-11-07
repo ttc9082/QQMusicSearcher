4c3d02cc2ccf4f5bfcdbdb82e942f53cf5bd186f
Tianqi <tongtianqi@kbstreet.com>
2015-11-07 18:14:59 +0800

    update debug

--- tongtianqi/settings.py
+++ tongtianqi/settings.py
@@ -27,6 +27,35 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'websocket':{
+            'format': '%(asctime)s %(process)d:%(name)s %(levelname)s: %(message)s'
+        }
+    },
+    'handlers': {
+        'file':{
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(os.getenv('TEMP') if (os.name == 'nt') else '/var/log/tongtianqi','tongtianqi.log'),
+            'maxBytes': 1024*1024*20,
+            'backupCount': 20,
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+        }
+    }
+}
+
 
 # Application definition
 
