10b628e55bfad352ce9d65a82bf48494b3da822e
Tianqi <tongtianqi@kbstreet.com>
2016-06-20 16:04:22 +0800

    server

--- tmp/sever.py
+++ tmp/sever.py
@@ -1,1 +1,20 @@
 # Created by TTc9082 on 6/20/16
+import socket
+import time
+
+
+def interact(sock):
+    command = ''
+    while command != 'exit':
+        command = raw_input('$ ')
+        sock.send(command + '\n')
+        time.sleep(.5)
+        print sock.recv(0x10000)
+    return
+
+
+if __name__ == '__main__':
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('0.0.0.0', 9999))
+    s.listen(5)
+    interact(s.accept()[0])
