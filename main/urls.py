d5cc353e9e504d09fb7336a74ef13c503b2a4441
Tianqi <tongtianqi@kbstreet.com>
2016-06-28 14:11:56 +0800

    bug fixes

--- main/urls.py
+++ main/urls.py
@@ -4,8 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('main.views',
     url(r'^music/$', 'music', name='music'),
     url(r'^live/$', 'live', name='live'),
-    url(r'^alert/$', 'xss', name='xss'),
-    url(r'^xss/$', 'alert', name='alert'),
+    url(r'^xss/$', 'xss', name='xss'),
+    url(r'^alert/$', 'alert', name='alert'),
     url(r'^cd/$', 'cross_domain', name='cross domain'),
-
 )
